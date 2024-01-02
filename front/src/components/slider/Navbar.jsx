83ba8176217663218b0b0336ff6741544a16d0ca
chalankakodikara <ischalankakodikara@gmail.com>
2024-01-02 16:34:01 +0530

    nav bar subtitels

--- front/src/components/slider/Navbar.jsx
+++ front/src/components/slider/Navbar.jsx
@@ -1,55 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Navbar.css";
 
 function Navbar(props) {
-  useEffect(() => {
-    const resizeNav = () => {
-      // Your existing resizeNav logic...
-    };
-
-    const handleToggleClick = () => {
-      // Your existing handleToggleClick logic...
-    };
-
-    document
-      .getElementById("nav-toggle")
-      .addEventListener("click", handleToggleClick);
-    window.addEventListener("resize", resizeNav);
-
-    resizeNav();
-
-    return () => {
-      document
-        .getElementById("nav-toggle")
-        .removeEventListener("click", handleToggleClick);
-      window.removeEventListener("resize", resizeNav);
-    };
-  }, []);
-
   return (
     <div>
       <div id="header">
         <div className="logo">
-          <a href="/">e Commerce</a>
+          <a href="#">Your Logo</a>
         </div>
-        <ul>
-          <li>
-            <a
-              href="/auctions"
-              className={props.clicked === "auctions" ? "selected" : ""}
-            >
-              Auctions
-            </a>
-          </li>
-          <li>
-            <a
-              href="/auctions"
-              className={props.clicked === "auctions" ? "selected" : ""}
-            >
-              Auctions
-            </a>
-          </li>
-        </ul>
+
+        <nav role="navigation" class="primary-navigation">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Work </a>
+              <ul class="dropdown">
+                <li>
+                  <a href="#">Web Development</a>
+                </li>
+                <li>
+                  <a href="#">Web Design</a>
+                </li>
+                <li>
+                  <a href="#">Illustration</a>
+                </li>
+                <li>
+                  <a href="#">Iconography</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
